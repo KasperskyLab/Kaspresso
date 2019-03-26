bb81e984ee9ae0cb26a354d78b25905437a4ce1f
matsyuk <Evgeny.Matsyuk@kaspersky.com>
2019-03-26 09:24:20 +0300

    refactroring of TestCases, SubCases, Scenarios

--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/scenarios/stepsrunners/exceptions/TestCaseStepsRunnerException.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/scenarios/stepsrunners/exceptions/TestCaseStepsRunnerException.kt
@@ -2,5 +2,4 @@ package com.kaspersky.kaspresso.scenarios.stepsrunners.exceptions
 
 import java.lang.RuntimeException
 
-class TestCaseStepsRunnerException(message: String?) : RuntimeException(message) {
-}
+class TestCaseStepsRunnerException(message: String?) : RuntimeException(message)
