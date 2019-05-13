90921651277a8b1f6456d4df2c22f21d535f9df8
a.blinov <a.blinov@hh.ru>
2019-05-13 13:41:59 +0300

    add sample app

---
+++ sample/src/main/kotlin/com/kaspersky/kaspressample/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.kaspersky.kaspressample
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class HomeFragment : Fragment() {
+    companion object {
+        fun newInstance() = HomeFragment()
+
+        const val TAG = "HomeFragment"
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
