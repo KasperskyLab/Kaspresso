3acb8827f373c861f8ff97b3a50bf0b1b882e34a
Andrey A. Antipov <Andrey.A.Antipov@kaspersky.com>
2019-05-31 15:08:19 +0300

    Merged PR 111514: attempts: remove overloads with seconds in arguments
    attempts: remove overloads with seconds in arguments

--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/extensions/kakaoext/KBaseViewExt.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/extensions/kakaoext/KBaseViewExt.kt
@@ -3,7 +3,6 @@ package com.kaspersky.kaspresso.extensions.kakaoext
 import com.kaspersky.kaspresso.configurator.Configurator
 import com.kaspersky.kaspresso.logger.UiTestLogger
 import com.kaspersky.klkakao.common.views.KBaseView
-import java.util.concurrent.TimeUnit
 
 /**
  * Provides an [com.kaspersky.kaspresso.flakysafety.attempt] method as an extension of [KBaseView].
@@ -26,20 +25,6 @@ fun <T : KBaseView<Any>, R> T.attempt(
 }
 
 /**
- * Provides a simplified [com.kaspersky.kaspresso.flakysafety.attempt] method as an extension of [KBaseView].
- */
-fun <T : KBaseView<Any>, R> T.attempt(
-    timeoutSec: Long,
-    action: T.() -> R
-): R {
-    return com.kaspersky.kaspresso.flakysafety.attempt(
-        timeoutMs = TimeUnit.SECONDS.toMillis(timeoutSec)
-    ) {
-        action.invoke(this)
-    }
-}
-
-/**
  * Provides a [com.kaspersky.kaspresso.flakysafety.wait] method as an extension of [KBaseView].
  */
 fun <T : KBaseView<Any>, R> T.wait(
@@ -50,20 +35,6 @@ fun <T : KBaseView<Any>, R> T.wait(
     return com.kaspersky.kaspresso.flakysafety.wait(
         timeoutMs = timeoutMs,
         logger = logger
-    ) {
-        action.invoke(this)
-    }
-}
-
-/**
- * Provides a simplified [com.kaspersky.kaspresso.flakysafety.wait] method as an extension of [KBaseView].
- */
-fun <T : KBaseView<Any>, R> T.wait(
-    timeoutSec: Long,
-    action: T.() -> R
-): R {
-    return com.kaspersky.kaspresso.flakysafety.wait(
-        timeoutMs = TimeUnit.SECONDS.toMillis(timeoutSec)
     ) {
         action.invoke(this)
     }
