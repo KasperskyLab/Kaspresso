05bcf812c4384db7173e4f4fa08e2ff1d91b466d
matsyuk <Evgeny.Matsyuk@kaspersky.com>
2019-06-06 13:25:38 +0300

    ConfiguratorExt comments

--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/configurator/ConfiguratorExt.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/configurator/ConfiguratorExt.kt
@@ -5,6 +5,10 @@ import android.support.test.espresso.PerformException
 import com.kaspersky.kaspresso.logger.UiTestLogger
 import com.kaspersky.kaspresso.logger.UiTestLoggerImpl
 
+/**
+ * Use this object only for extensions and nothing else!
+ * In other cases use [Configurator]
+ */
 internal object ConfiguratorExt {
 
     var attemptsTimeoutMs: Long = Configurator.DEFAULT_ATTEMPTS_TIMEOUT_MS
