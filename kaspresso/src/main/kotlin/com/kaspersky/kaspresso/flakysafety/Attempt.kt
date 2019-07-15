7dd89557a697477ecd527dc8cdde775682d8207c
matsyuk <Evgeny.Matsyuk@kaspersky.com>
2019-07-15 11:46:42 +0300

    We have added isAndroidSystemHandling param in attempt() method

--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/flakysafety/Attempt.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/flakysafety/Attempt.kt
@@ -1,5 +1,11 @@
 package com.kaspersky.kaspresso.flakysafety
 
+import android.support.test.InstrumentationRegistry
+import android.support.test.uiautomator.By
+import android.support.test.uiautomator.BySelector
+import android.support.test.uiautomator.UiDevice
+import android.support.test.uiautomator.Until
+import android.widget.FrameLayout
 import com.kaspersky.kaspresso.configurator.Configurator
 import com.kaspersky.kaspresso.exceptions.KaspressoAssertionError
 import com.kaspersky.kaspresso.extensions.other.getStackTraceAsString
@@ -13,6 +19,10 @@ import com.kaspersky.kaspresso.logger.UiTestLogger
  * @param message a message of an AssertionError if it's set to anything but null.
  * @param logger a logger to log errors.
  * @param allowedExceptions exceptions that doesn't stop attempts.
+ * @param isAndroidSystemHandling is attempt() method handling android system dialogs/windows.
+ * If the param is true (by default the param is true) and the android system dialog was detected
+ * then we try to remove detected dialog and rerun attempt() with
+ * timeoutMs=Configurator.attemptsTimeoutMs and intervalMs=Configurator.attemptsIntervalMs
  * @param action an action that is attempted to be invoked.
  * @return [T] as it is a result of [action] invocation.
  */
@@ -22,6 +32,7 @@ fun <T> attempt(
     message: String? = null,
     logger: UiTestLogger = Configurator.logger,
     allowedExceptions: Set<Class<out Throwable>> = Configurator.allowedExceptionsForAttempt,
+    isAndroidSystemHandling: Boolean = true,
     action: () -> T
 ) {
     if (intervalMs >= timeoutMs) {
@@ -51,6 +62,26 @@ fun <T> attempt(
         }
     } while (System.currentTimeMillis() - startTime <= timeoutMs)
 
+    with(UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())) {
+        if (isVisible(By.pkg("android").clazz(FrameLayout::class.java))) {
+            logger.i("attempt() method. The android system dialog/window was detected. " +
+                    "Kaspresso presses back and tries to rerun attempt() with timeoutMS=${Configurator.attemptsTimeoutMs} " +
+                    "and intervalMs=${Configurator.attemptsIntervalMs}"
+            )
+            pressBack()
+            attempt(
+                timeoutMs = Configurator.attemptsTimeoutMs,
+                intervalMs = Configurator.attemptsIntervalMs,
+                message = message,
+                logger = logger,
+                allowedExceptions = allowedExceptions,
+                isAndroidSystemHandling = isAndroidSystemHandling,
+                action = action
+            )
+            return
+        }
+    }
+
     logger.e(
         "All attempts to interact for $timeoutMs ms totally failed " +
                 "because of ${caughtAllowedException.javaClass.simpleName}"
@@ -70,4 +101,12 @@ private fun failAttempt(
 ): Nothing {
     message ?: throw caughtAllowedException
     throw KaspressoAssertionError(message, caughtAllowedException)
+}
+
+/**
+ * isVisible method for non-app's elements and with a waiting
+ */
+private fun UiDevice.isVisible(selector: BySelector, timeMs: Long = 1000): Boolean {
+    wait(Until.findObject(selector), timeMs)
+    return findObject(selector) != null
 }
