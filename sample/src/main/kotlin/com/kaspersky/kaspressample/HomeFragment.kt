d840fc2bc0438db8590b5ac89e7ef20cc284ef59
Alexander Blinov <xanderblinov@gmail.com>
2019-07-30 20:17:37 +0300

    sample detekt

--- sample/src/main/kotlin/com/kaspersky/kaspressample/HomeFragment.kt
+++ sample/src/main/kotlin/com/kaspersky/kaspressample/HomeFragment.kt
@@ -1,10 +1,10 @@
 package com.kaspersky.kaspressample
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 class HomeFragment : Fragment() {
     companion object {
--- sample/src/main/kotlin/com/kaspersky/kaspressample/MainActivity.kt
+++ sample/src/main/kotlin/com/kaspersky/kaspressample/MainActivity.kt
@@ -2,8 +2,8 @@ package com.kaspersky.kaspressample
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
