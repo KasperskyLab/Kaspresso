4972e6dc9c13249edd689331970cf77dc6d4593e
Sergey Zankin <sergey.zankin@kaspersky.com>
2019-08-20 11:07:12 +0300

    add support android 10 permission dialog

--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/device/permissions/PermissionsImpl.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/device/permissions/PermissionsImpl.kt
@@ -16,35 +16,44 @@ class PermissionsImpl(
 ) : Permissions {
 
     private val packageInstallerPackageName = "com.android.packageinstaller"
+
     private val permissionDenyButtonId = "$packageInstallerPackageName:id/permission_deny_button"
     private val permissionAllowButtonId = "$packageInstallerPackageName:id/permission_allow_button"
+
+    private val buttonResNameMap = mapOf(
+        Permissions.Button.ALLOW to getResIdWithPackageName("permission_allow_button"),
+        Permissions.Button.ALLOW_ALWAYS to getResIdWithPackageName("permission_allow_always_button"),
+        Permissions.Button.ALLOW_FOREGROUND to getResIdWithPackageName("permission_allow_foreground_only_button"),
+        Permissions.Button.DENY to getResIdWithPackageName("permission_deny_button"))
 
     /**
      * Waits for 1 sec, passes the permission-requesting permissions dialog and allows permissions.
      */
-    override fun allowViaDialog() = wait(timeoutMs = 1_000) { handlePermissionRequest(shouldAllowPermissions = true) }
+    override fun allowViaDialog() = wait(timeoutMs = 1_000) { handlePermissionRequest(permissionAllowButtonId) }
 
     /**
      * Waits for 1 sec, passes the permission-requesting permissions dialog and denies permissions.
      */
-    override fun denyViaDialog() = wait(timeoutMs = 1_000) { handlePermissionRequest(shouldAllowPermissions = false) }
+    override fun denyViaDialog() = wait(timeoutMs = 1_000) { handlePermissionRequest(permissionDenyButtonId) }
+
+    /**
+     * Waits for 1 sec, passes the permission-requesting permissions dialog
+     */
+    override fun clickOn(button: Permissions.Button) = wait(timeoutMs = 1_000) {
+        handlePermissionRequest(buttonResNameMap[button])
+    }
 
     /**
      * Passes the permission-requesting permissions dialog.
      *
-     * @param shouldAllowPermissions if set to true permissions will be allowed, otherwise will not.
+     * @param buttonResId resource name of permission dialog button
      */
-    private fun handlePermissionRequest(shouldAllowPermissions: Boolean) {
+    private fun handlePermissionRequest(buttonResId: String?) {
         try {
             val btnSelector = UiSelector()
                 .clickable(true)
                 .checkable(false)
-                .resourceId(
-                    if (shouldAllowPermissions)
-                        permissionAllowButtonId
-                    else
-                        permissionDenyButtonId
-                )
+                .resourceId(buttonResId)
 
             val btn = uiDevice.findObject(btnSelector)
 
@@ -57,5 +66,10 @@ class PermissionsImpl(
             logger.e(e.getStackTraceAsString())
             throw e
         }
+    }
+
+    private fun getResIdWithPackageName(resId: String): String {
+        val packageName = "com.google.android.permissioncontroller"
+        return "$packageName:id/$resId"
     }
 }
