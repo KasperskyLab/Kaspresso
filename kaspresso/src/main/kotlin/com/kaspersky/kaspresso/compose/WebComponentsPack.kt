0caa96952c18444aaa39d29c102333b488374ff5
eakurnikov <egor.kurnikov@kaspersky.com>
2019-09-05 15:27:46 +0300

    review fixes

--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/compose/WebComponentsPack.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/compose/WebComponentsPack.kt
@@ -4,30 +4,11 @@ import androidx.test.espresso.web.webdriver.Locator
 import com.agoda.kakao.web.WebElementBuilder
 import java.lang.IllegalArgumentException
 
-data class Component<T>(
-    val element: T,
-    val action: T.() -> Unit
-)
-
 data class WebComponent(
     val webElement: WebElementBuilder.KWebInteraction,
     val webElementBuilder: WebElementBuilder,
     val webAction: WebElementBuilder.KWebInteraction.() -> Unit
 )
-
-class ComponentPack<T> {
-
-    private val components: MutableList<Component<T>> = arrayListOf()
-
-    fun or(element: T, action: T.() -> Unit) {
-        components += Component(element, action)
-    }
-
-    internal fun build(): MutableList<Component<T>> {
-        if (components.isEmpty()) throw IllegalArgumentException("Nothing to compose")
-        return components
-    }
-}
 
 class WebComponentPack(
     private val webElementBuilder: WebElementBuilder
