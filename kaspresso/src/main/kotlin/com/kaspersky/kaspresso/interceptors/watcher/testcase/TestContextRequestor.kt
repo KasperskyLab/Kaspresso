190838c644a7f980070f0866a214b85c226e6ec4
matsyuk <Evgeny.Matsyuk@kaspersky.com>
2019-11-25 17:28:44 +0300

    have added some comments

--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/interceptors/watcher/testcase/TestContextRequestor.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/interceptors/watcher/testcase/TestContextRequestor.kt
@@ -2,6 +2,9 @@ package com.kaspersky.kaspresso.interceptors.watcher.testcase
 
 import com.kaspersky.kaspresso.testcases.core.testcontext.BaseTestContext
 
+/**
+ * Requestor of BaseTestContext
+ */
 interface TestContextRequestor {
 
     fun requestBaseTestContext(context: BaseTestContext) = Unit
