d1f12379155080cad5f4fda5e0caf73d1f02293e
RuslanMingaliev <mingaliev.rr@gmail.com>
2019-11-25 09:51:29 +0300

    detekt issues fix

--- foreign-process-tests/src/main/java/com/kaspersky/kaspresso/foreign_process_tests/MainActivity.kt
+++ foreign-process-tests/src/main/java/com/kaspersky/kaspresso/foreign_process_tests/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.kaspersky.kaspresso.foreign_process_tests
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
