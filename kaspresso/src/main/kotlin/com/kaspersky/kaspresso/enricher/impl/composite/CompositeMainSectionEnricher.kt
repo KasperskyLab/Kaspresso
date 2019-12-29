8acd601e71a6a6da36ff75ab3356c77b4527ef73
Pavel Strelchenko <p.strelchenko@hh.ru>
2019-12-30 00:31:02 +0400

    [feature/main_section_enricher] -- Add composite implementation of enricher

---
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/enricher/impl/composite/CompositeMainSectionEnricher.kt
@@ -0,0 +1,27 @@
+package com.kaspersky.kaspresso.enricher.impl.composite
+
+
+import com.kaspersky.kaspresso.enricher.MainSectionEnricher
+import com.kaspersky.kaspresso.internal.extensions.other.forEachSafely
+import com.kaspersky.kaspresso.testcases.core.testcontext.TestContext
+import com.kaspersky.kaspresso.testcases.models.info.TestInfo
+
+/**
+ * The implementation of the [MainSectionEnricher] interface.
+ * Composes all of [MainSectionEnricher]s list into one composite [MainSectionEnricher] that is actually
+ * called by [com.kaspersky.kaspresso.testcases.core.TestRunner] on each test event.
+ */
+class CompositeMainSectionEnricher<Data>(
+    private val mainSectionEnrichers: List<MainSectionEnricher<Data>>,
+    private val exceptions: MutableList<Throwable>
+) : MainSectionEnricher<Data> {
+
+    override fun beforeMainSectionRun(testInfo: TestInfo, testContext: TestContext<Data>) {
+        mainSectionEnrichers.forEachSafely(exceptions) { it.beforeMainSectionRun(testInfo, testContext) }
+    }
+
+    override fun afterMainSectionRun(testInfo: TestInfo, testContext: TestContext<Data>) {
+        mainSectionEnrichers.forEachSafely(exceptions) { it.afterMainSectionRun(testInfo, testContext) }
+    }
+
+}
