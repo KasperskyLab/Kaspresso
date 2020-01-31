b9dc9b19e214d3193b99cf5a902d9d101b15354a
matsyuk <Evgeny.Matsyuk@kaspersky.com>
2020-01-31 17:50:47 +0300

    feature=kas_kau_improvements: DeviceLanguageSampleTest

---
+++ sample/src/androidTest/java/com/kaspersky/kaspressample/device_tests/DeviceLanguageSampleTest.kt
@@ -0,0 +1,57 @@
+package com.kaspersky.kaspressample.device_tests
+
+import android.Manifest
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import androidx.test.rule.GrantPermissionRule
+import com.kaspersky.kaspressample.MainActivity
+import com.kaspersky.kaspressample.screen.MainScreen
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.*
+
+@RunWith(AndroidJUnit4::class)
+class DeviceLanguageSampleTest : TestCase() {
+
+    @get:Rule
+    val runtimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
+
+    @get:Rule
+    val activityTestRule = ActivityTestRule(MainActivity::class.java, true, true)
+
+    @Test
+    fun keyboardSampleTest() {
+        run {
+
+            step("Start MainScreen in default locale") {
+                MainScreen {
+                    simpleButton {
+                        isVisible()
+                        containsText("Simple fragment")
+                    }
+                }
+            }
+
+            step("change language") {
+                device.language.switchInApp(Locale("ru"))
+//                activityTestRule.finishActivity()
+                activityTestRule.launchActivity(null)
+                Thread.sleep(5_000)
+            }
+
+            step("Start MainScreen in russian locale") {
+                MainScreen {
+                    simpleButton {
+                        isVisible()
+                        containsText("Простой пример")
+                    }
+                }
+            }
+        }
+    }
+}
