6a5857432991106335c7d202ddbd8b5f38516c8a
Ruslan Mingaliev <mingaliev.rr@gmail.com>
2021-05-26 12:28:44 +0300

    Migrate to .kts script
    Signed-off-by: Ruslan Mingaliev <mingaliev.rr@gmail.com>

---
+++ tutorials/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdkVersion(30)
+
+    defaultConfig {
+        applicationId = "com.kaspersky.kaspresso.tutorials"
+        minSdkVersion(16)
+        targetSdkVersion(30)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.5.0")
+    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("com.google.android.material:material:1.3.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+
+    androidTestImplementation("androidx.test.ext:junit:1.1.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+}
