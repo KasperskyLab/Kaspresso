30b4fc15e4d30eeedf1a14bd4a088680c9e63a36
Ruslan Mingaliev <mingaliev.rr@gmail.com>
2021-05-31 17:00:08 +0300

    Fix kakao version
    Signed-off-by: Ruslan Mingaliev <mingaliev.rr@gmail.com>

--- tutorial/src/main/kotlin/com/kaspersky/kaspresso/tutorial/simple/SimpleActivity.kt
+++ tutorial/src/main/kotlin/com/kaspersky/kaspresso/tutorial/simple/SimpleActivity.kt
@@ -1,17 +1,20 @@
 package com.kaspersky.kaspresso.tutorial.simple
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.kaspersky.kaspresso.tutorial.R
 import kotlinx.android.synthetic.main.activity_simple.*
-import java.util.concurrent.Executors
 
 class SimpleActivity : AppCompatActivity() {
 
     companion object {
         private const val TIMEOUT: Long = 2000
     }
+
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,10 +28,7 @@ class SimpleActivity : AppCompatActivity() {
         }
         button_2.setOnClickListener {
             // special sleep to emulate ui block operation to check attempt method correctness
-            Executors.newSingleThreadExecutor().submit {
-                Thread.sleep(TIMEOUT)
-                runOnUiThread { edit.visibility = View.VISIBLE }
-            }
+            handler.postDelayed({ edit.visibility = View.VISIBLE }, TIMEOUT)
         }
     }
 }
