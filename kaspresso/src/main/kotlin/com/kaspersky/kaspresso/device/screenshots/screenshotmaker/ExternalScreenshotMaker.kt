92f3072aa9059de924a029d41a08d92965993a92
eakurnikov <eakurnikov@gmail.com>
2021-06-03 14:50:21 +0300

    screenshot feature updated to use new file managers, screenshot quality param added

--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/device/screenshots/screenshotmaker/ExternalScreenshotMaker.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/device/screenshots/screenshotmaker/ExternalScreenshotMaker.kt
@@ -2,14 +2,19 @@ package com.kaspersky.kaspresso.device.screenshots.screenshotmaker
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
+import com.kaspersky.kaspresso.params.ScreenshotParams
 import java.io.File
 
 /**
  * Captures spoon-compatible screenshots by uiautomator.
  */
-class ExternalScreenshotMaker : ScreenshotMaker {
+class ExternalScreenshotMaker(
+    private val params: ScreenshotParams = ScreenshotParams()
+) : ScreenshotMaker {
 
     override fun takeScreenshot(file: File) {
-        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).takeScreenshot(file)
+        UiDevice.getInstance(
+            InstrumentationRegistry.getInstrumentation()
+        ).takeScreenshot(file, 1.0f, params.quality)
     }
 }
