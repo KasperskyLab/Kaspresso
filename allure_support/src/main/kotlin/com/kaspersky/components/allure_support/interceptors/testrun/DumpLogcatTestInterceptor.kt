e700b34aeb9100c798212bcc255393361f217edb
eakurnikov <eakurnikov@gmail.com>
2021-06-03 15:20:24 +0300

    allure supporting interceptors added

---
+++ allure_support/src/main/kotlin/com/kaspersky/components/allure_support/interceptors/testrun/DumpLogcatTestInterceptor.kt
@@ -0,0 +1,15 @@
+package com.kaspersky.components.allure_support.interceptors.testrun
+
+import com.kaspersky.kaspresso.interceptors.watcher.testcase.TestRunWatcherInterceptor
+import com.kaspersky.kaspresso.testcases.models.info.TestInfo
+import com.kaspersky.components.allure_support.files.attachLogcatToAllureReport
+import com.kaspersky.kaspresso.device.logcat.dumper.LogcatDumper
+
+class DumpLogcatTestInterceptor(
+    private val logcatDumper: LogcatDumper
+) : TestRunWatcherInterceptor {
+
+    override fun onTestFinished(testInfo: TestInfo, success: Boolean) {
+        logcatDumper.dumpAndApply("TestLogcat") { attachLogcatToAllureReport() }
+    }
+}
