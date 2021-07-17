c2a5edff145eb3989d5ce5c4dd4778f1c4b8f127
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2021-07-17 21:51:28 +0200

    Add Shared test that can run as Instrumentation and JVM test
    This text contains very long text. Therefore, it needs to use the AutoScrollInterceptor to run successfully as Instrumentation as well as JVM test

--- samples/kaspresso-sample/src/sharedTest/kotlin/com/kaspersky/kaspressample/screen/RobolectricScreen.kt
+++ samples/kaspresso-sample/src/sharedTest/kotlin/com/kaspersky/kaspressample/screen/RobolectricScreen.kt
@@ -4,7 +4,6 @@ import com.kaspersky.kaspressample.R
 import com.kaspersky.kaspressample.robolectric.RobolectricActivity
 import com.kaspersky.kaspresso.screens.KScreen
 import io.github.kakaocup.kakao.edit.KEditText
-import io.github.kakaocup.kakao.scroll.KScrollView
 import io.github.kakaocup.kakao.text.KButton
 
 class RobolectricScreen : KScreen<RobolectricScreen>() {
@@ -14,39 +13,9 @@ class RobolectricScreen : KScreen<RobolectricScreen>() {
 
     val findMeButton = KButton { withId(R.id.findMeButton) }
 
-    val scrollView = KScrollView { withId(R.id.scrollMe) }
-
     val firstNameEditText = KEditText { withId(R.id.firstName) }
     val lastNameEditText = KEditText { withId(R.id.lastName) }
     val ageEditText = KEditText { withId(R.id.age) }
 
     val maleButton = KButton { withId(R.id.male) }
-
-    fun writeFirstName(firstName: String) {
-        firstNameEditText.replaceText(firstName)
-    }
-
-    fun writeLastName(lastName: String) {
-        lastNameEditText.replaceText(lastName)
-    }
-
-    fun writeAge(age: String) {
-        ageEditText.replaceText(age)
-    }
-
-    fun clickOnMale() {
-        maleButton.click()
-    }
-
-    fun clickOnFindMeButton() {
-        findMeButton.click()
-    }
-
-    fun verifyFirstNameIs(firstName: String) {
-        firstNameEditText.hasText(firstName)
-    }
-
-    fun isButtonDisplayed() {
-        findMeButton.isDisplayed()
-    }
 }
