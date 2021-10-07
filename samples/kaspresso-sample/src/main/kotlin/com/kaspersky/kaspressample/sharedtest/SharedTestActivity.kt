653974d6f5296e915f54e81c9f05fbbfd5dfa65c
eugene_matsyuk <eugene.matsyuk@gmail.com>
2021-10-07 06:47:50 +0300

    issue #242. timeout to check flakySafety on the JVM was added

--- samples/kaspresso-sample/src/main/kotlin/com/kaspersky/kaspressample/sharedtest/SharedTestActivity.kt
+++ samples/kaspresso-sample/src/main/kotlin/com/kaspersky/kaspressample/sharedtest/SharedTestActivity.kt
@@ -1,14 +1,23 @@
 package com.kaspersky.kaspressample.sharedtest
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.kaspersky.kaspressample.R
+import kotlinx.android.synthetic.main.activity_sharedtest.*
+import kotlinx.coroutines.delay
 
 class SharedTestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sharedtest)
+
+        lifecycleScope.launchWhenResumed {
+            delay(2000)
+            toggleButton.visibility = View.VISIBLE
+        }
     }
 }
 
