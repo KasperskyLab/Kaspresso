e2e6b2497b0b23c5064b058439e1257fb5e7c49a
Eugene Matsyuk <matzuk2@mail.ru>
2021-10-08 04:50:06 +0300

    Update allure-support/src/main/kotlin/com/kaspersky/components/alluresupport/AllureSupportKaspressoBuilder.kt
    Co-authored-by: Ruslan Mingaliev <mingaliev.rr@gmail.com>

--- allure-support/src/main/kotlin/com/kaspersky/components/alluresupport/AllureSupportKaspressoBuilder.kt
+++ allure-support/src/main/kotlin/com/kaspersky/components/alluresupport/AllureSupportKaspressoBuilder.kt
@@ -12,7 +12,7 @@ import com.kaspersky.kaspresso.kaspresso.Kaspresso
  * Kaspresso Builder that includes all appropriate interceptors to support rich Allure reports.
  *
  * If a test is executing on the JVM (with Robolectric) environment then mentioned above interceptors are not including to prevent crashes.
- * Sure, Allure reports don't have any sense in non Instrumental environment.
+ * Allure reports don't have any sense in non Instrumental environment.
  */
 fun Kaspresso.Builder.Companion.withAllureSupport(
     customize: Kaspresso.Builder.() -> Unit = {}
