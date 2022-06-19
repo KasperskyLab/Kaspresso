0b8199154e305ac2afddf186e7a5ab5ba4c3ca2a
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2022-06-19 22:39:16 +0200

    Replace ActivityTestRule in Kautomator-sample module

--- samples/kautomator-sample/src/androidTest/java/com/kaspersky/kaspresso/kautomatorsample/test/components/AlertDialogTest.kt
+++ samples/kautomator-sample/src/androidTest/java/com/kaspersky/kaspresso/kautomatorsample/test/components/AlertDialogTest.kt
@@ -1,6 +1,6 @@
 package com.kaspersky.kaspresso.kautomatorsample.test.components
 
-import androidx.test.rule.ActivityTestRule
+import androidx.test.ext.junit.rules.activityScenarioRule
 import com.kaspersky.kaspresso.kautomatorsample.ComponentsActivity
 import com.kaspersky.kaspresso.kautomatorsample.screen.ComponentsScreen
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
@@ -10,61 +10,59 @@ import org.junit.Test
 class AlertDialogTest : TestCase() {
 
     @get:Rule
-    val activityRule = ActivityTestRule(ComponentsActivity::class.java, true, true)
+    val activityRule = activityScenarioRule<ComponentsActivity>()
 
     @Test
-    fun test() {
-        run {
-            step("Open dialog") {
-                ComponentsScreen {
-                    showDialogBtn {
-                        click()
+    fun test() = run {
+        step("Open dialog") {
+            ComponentsScreen {
+                showDialogBtn {
+                    click()
+                }
+            }
+        }
+
+        step("Check title and message") {
+            ComponentsScreen {
+                dialog {
+                    title {
+                        hasText("Title")
+                    }
+
+                    message {
+                        hasText("Message")
+                    }
+
+                    positiveButton {
+                        longClick()
                     }
                 }
             }
+        }
 
-            step("Check title and message") {
-                ComponentsScreen {
-                    dialog {
-                        title {
-                            hasText("Title")
-                        }
+        step("Negative button click") {
+            ComponentsScreen {
+                showDialogBtn {
+                    click()
+                }
 
-                        message {
-                            hasText("Message")
-                        }
-
-                        positiveButton {
-                            longClick()
-                        }
+                dialog {
+                    negativeButton {
+                        longClick()
                     }
                 }
             }
+        }
 
-            step("Negative button click") {
-                ComponentsScreen {
-                    showDialogBtn {
-                        click()
-                    }
-
-                    dialog {
-                        negativeButton {
-                            longClick()
-                        }
-                    }
+        step("Neutral button click") {
+            ComponentsScreen {
+                showDialogBtn {
+                    click()
                 }
-            }
 
-            step("Neutral button click") {
-                ComponentsScreen {
-                    showDialogBtn {
-                        click()
-                    }
-
-                    dialog {
-                        neutralButton {
-                            longClick()
-                        }
+                dialog {
+                    neutralButton {
+                        longClick()
                     }
                 }
             }
