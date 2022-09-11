28cc196a7d90948a8f97a442b1f162165e3df427
Ruslan Mingaliev <mingaliev.rr@gmail.com>
2022-09-12 02:10:41 +0300

    Update kotlin language version
    Signed-off-by: Ruslan Mingaliev <mingaliev.rr@gmail.com>

--- build-logic/kotlin/src/main/kotlin/convention.kotlin-base.gradle.kts
+++ build-logic/kotlin/src/main/kotlin/convention.kotlin-base.gradle.kts
@@ -1,20 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlinLanguageVersion = "1.6"
+val kotlinLanguageVersion = "1.7"
 
 tasks.withType<KotlinCompile>().configureEach {
     kotlinOptions {
 
         jvmTarget = JavaVersion.VERSION_1_8.toString()
 
-//        TODO: remove `android-extensions` plugin before
+//        TODO: fix all warnings before
 //        allWarningsAsErrors = true
 
         languageVersion = kotlinLanguageVersion
         apiVersion = kotlinLanguageVersion
 
         freeCompilerArgs = freeCompilerArgs +
-                "-Xopt-in=kotlin.RequiresOptIn" +
+                "-opt-in=kotlin.RequiresOptIn" +
                 "-progressive"
     }
 }
