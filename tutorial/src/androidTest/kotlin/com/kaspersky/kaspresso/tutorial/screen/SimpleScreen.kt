822ded201c317e29bfac3c74c417b278362bc053
Azamat <Cherchesov>
2022-10-19 00:48:51 +0300

    added more details for first simple test

--- tutorial/src/androidTest/kotlin/com/kaspersky/kaspresso/tutorial/screen/SimpleScreen.kt
+++ tutorial/src/androidTest/kotlin/com/kaspersky/kaspresso/tutorial/screen/SimpleScreen.kt
@@ -5,13 +5,14 @@ import com.kaspersky.kaspresso.tutorial.R
 import com.kaspersky.kaspresso.tutorial.simple.SimpleActivity
 import io.github.kakaocup.kakao.edit.KEditText
 import io.github.kakaocup.kakao.text.KButton
+import io.github.kakaocup.kakao.text.KTextView
 
 object SimpleScreen : KScreen<SimpleScreen>() {
 
     override val layoutId: Int = R.layout.activity_simple
     override val viewClass: Class<*> = SimpleActivity::class.java
 
-    val title = KButton { withId(R.id.simple_title) }
+    val title = KTextView { withId(R.id.simple_title) }
     val button = KButton { withId(R.id.change_title_btn) }
     val input = KEditText { withId(R.id.input_text) }
 }
