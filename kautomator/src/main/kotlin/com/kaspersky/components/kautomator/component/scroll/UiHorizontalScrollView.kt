ed48e1780ed13c43d86db497a5eb5fbe3cf01e2e
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2022-11-14 17:26:07 +0100

    solve detekt issues

--- kautomator/src/main/kotlin/com/kaspersky/components/kautomator/component/scroll/UiHorizontalScrollView.kt
+++ kautomator/src/main/kotlin/com/kaspersky/components/kautomator/component/scroll/UiHorizontalScrollView.kt
@@ -1,5 +1,4 @@
 @file:Suppress("unused")
-
 package com.kaspersky.components.kautomator.component.scroll
 
 import androidx.test.uiautomator.UiScrollable
@@ -9,11 +8,10 @@ import com.kaspersky.components.kautomator.component.common.builders.UiViewBuild
 import com.kaspersky.components.kautomator.component.common.builders.UiViewSelector
 import com.kaspersky.components.kautomator.component.common.views.UiBaseView
 
-class UiHorizontalScrollView: UiBaseView<UiScrollView>, UiSwipeableActions, UiScrollableActions {
+class UiHorizontalScrollView : UiBaseView<UiScrollView>, UiSwipeableActions, UiScrollableActions {
     constructor(selector: UiViewSelector) : super(selector)
     constructor(builder: UiViewBuilder.() -> Unit) : super(builder)
 
     override val setUiScrollableOrientation: UiScrollable.() -> UiScrollable
         get() = { setAsHorizontalList() }
 }
-
--- samples/kautomator-sample/src/androidTest/java/com/kaspersky/kaspresso/kautomatorsample/screen/NestedScrollViewsScreen.kt
+++ samples/kautomator-sample/src/androidTest/java/com/kaspersky/kaspresso/kautomatorsample/screen/NestedScrollViewsScreen.kt
@@ -4,13 +4,13 @@ import com.kaspersky.components.kautomator.component.scroll.UiHorizontalScrollVi
 import com.kaspersky.components.kautomator.component.text.UiButton
 import com.kaspersky.components.kautomator.screen.UiScreen
 
-object NestedScrollViewsScreen: UiScreen<NestedScrollViewsScreen>() {
+object NestedScrollViewsScreen : UiScreen<NestedScrollViewsScreen>() {
 
     override val packageName: String = "com.kaspersky.kaspresso.kautomatorsample"
 
-    val hscrollView = UiHorizontalScrollView { withId(this@NestedScrollViewsScreen.packageName,"hscrollView")}
-    val nnHscrollView = UiHorizontalScrollView { withId(this@NestedScrollViewsScreen.packageName,"nnHscrollView")}
-    val hbutton1 = UiButton { withId(this@NestedScrollViewsScreen.packageName,"hvText1") }
+    val hscrollView = UiHorizontalScrollView { withId(this@NestedScrollViewsScreen.packageName, "hscrollView") }
+    val nnHscrollView = UiHorizontalScrollView { withId(this@NestedScrollViewsScreen.packageName, "nnHscrollView") }
+    val hbutton1 = UiButton { withId(this@NestedScrollViewsScreen.packageName, "hvText1") }
     val hbutton7 = UiButton { withId(this@NestedScrollViewsScreen.packageName, "hvText7") }
     val button1 = UiButton { withId(this@NestedScrollViewsScreen.packageName, "tvText1") }
     val button20 = UiButton { withId(this@NestedScrollViewsScreen.packageName, "tvText20") }
