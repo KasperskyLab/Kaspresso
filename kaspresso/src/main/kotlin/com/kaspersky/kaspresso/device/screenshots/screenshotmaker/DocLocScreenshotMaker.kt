53d326860a2890294619518dbc06cdb9b26cde91
Ilya Dziamidovich <Ilya.Demidovich@kaspersky.com>
2022-11-15 13:30:48 +0300

    add comments to doc loc maker

--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/device/screenshots/screenshotmaker/DocLocScreenshotMaker.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/device/screenshots/screenshotmaker/DocLocScreenshotMaker.kt
@@ -2,6 +2,9 @@ package com.kaspersky.kaspresso.device.screenshots.screenshotmaker
 
 import java.io.File
 
+/**
+ * Calls [screenshotMaker] on default screenshots and [fullWindowScreenshotMaker] on full window screenshots
+ */
 class DocLocScreenshotMaker(
     private val screenshotMaker: ScreenshotMaker,
     private val fullWindowScreenshotMaker: ScreenshotMaker
