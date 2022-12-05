4eec2c31045b7e2d409d1586eb9d8287d43226ec
Azamat <a1tt@rambler.ru>
2022-12-05 20:06:54 +0300

    ISSUE-404: moved Cache back to KaspressoLateRunListener.kt

--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/runner/listener/KaspressoLateRunListener.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/runner/listener/KaspressoLateRunListener.kt
@@ -1,5 +1,7 @@
 package com.kaspersky.kaspresso.runner.listener
 
+import org.junit.runner.Description
+
 /**
  * Listener added in runtime. When test tun listeners are added in runtime it won't
  * call testRunStarted and testStarted because it was added to late. Thus it's called
@@ -16,8 +18,33 @@ interface KaspressoLateRunListener : KaspressoRunListener {
      *
      * @param cache - callbacks that were called before KaspressoLateRunListener was added to runner
      */
-    fun lateInit(cache: KaspressoRunNotifier.Cache) {
+    fun lateInit(cache: Cache) {
         testRunStarted(cache.testRunStartedDescription)
         testStarted(cache.firstTestStartedDescription)
+    }
+
+    /**
+     * "Late" listeners don't receive first testRunStarted and testStarted calls.
+     * This cache is used to store the first test and the first test run descriptions
+     * to pass them into late listeners later
+     */
+    class Cache {
+        lateinit var testRunStartedDescription: Description
+            private set
+
+        lateinit var firstTestStartedDescription: Description
+            private set
+
+        fun testRunStarted(description: Description) {
+            if (!::testRunStartedDescription.isInitialized) {
+                testRunStartedDescription = description
+            }
+        }
+
+        fun testStarted(description: Description) {
+            if (!::firstTestStartedDescription.isInitialized) {
+                firstTestStartedDescription = description
+            }
+        }
     }
 }
