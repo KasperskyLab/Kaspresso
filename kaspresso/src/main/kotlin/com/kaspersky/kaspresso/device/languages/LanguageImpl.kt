a762fca3cf8517fa5cb4ff274a6ecb524fc24bb6
sumin93 <suminfx@mail.ru>
2023-06-21 20:55:37 +0300

    TECH: add explanation to exception

--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/device/languages/LanguageImpl.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/device/languages/LanguageImpl.kt
@@ -28,7 +28,15 @@ class LanguageImpl(
         try {
             applyCurrentLocaleToContext(locale = locale)
             logger.i("Switch the language in the Application to $locale: success")
-        } catch (error: Throwable) {
+        }
+        catch (reflectiveException: NoSuchFieldException) {
+            val message = """
+                For in-app switching language you should use at least 1.6.0 version of appcompat library.
+                Please find this dependency and increase version to androidx.appcompat:appcompat:1.6.0 or higher
+                """.trimIndent()
+            throw RuntimeException(message)
+        }
+        catch (error: Throwable) {
             logger.e("Switch the language in the Application to $locale: failed with the error: $error")
             throw error
         }
