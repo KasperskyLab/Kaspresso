23d6ef8f949f7369f25d1c71e8369d438c670764
sumin93 <suminfx@mail.ru>
2023-06-26 11:48:06 +0300

    TECH: fix switching dark mode

--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/docloc/rule/ToggleNightModeRule.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/docloc/rule/ToggleNightModeRule.kt
@@ -1,12 +1,15 @@
 package com.kaspersky.kaspresso.docloc.rule
 
+import android.os.Handler
 import androidx.appcompat.app.AppCompatDelegate
+import com.kaspersky.kaspresso.device.Device
 import com.kaspersky.kaspresso.logger.UiTestLogger
 import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
 
 class ToggleNightModeRule internal constructor(
+    private val device: Device,
     private val toggleNightMode: Boolean,
     private val logger: UiTestLogger
 ) : TestRule {
@@ -14,21 +17,28 @@ class ToggleNightModeRule internal constructor(
     var isNightMode: Boolean = false
         private set
 
+    private fun turnNightMode(turnOn: Boolean) {
+        val mode = if (turnOn) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+        Handler(device.targetContext.mainLooper).post {
+            AppCompatDelegate.setDefaultNightMode(mode)
+        }
+    }
+
     override fun apply(base: Statement, description: Description): Statement {
         return object : Statement() {
             override fun evaluate() {
                 logger.i("DocLoc: ToggleNightModeRule started")
                 try {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    turnNightMode(false)
                     isNightMode = false
                     base.evaluate()
                     if (toggleNightMode) {
-                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                        turnNightMode(true)
                         isNightMode = true
                         base.evaluate()
                     }
                 } finally {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_UNSPECIFIED)
+                    turnNightMode(false)
                     isNightMode = false
                 }
             }
