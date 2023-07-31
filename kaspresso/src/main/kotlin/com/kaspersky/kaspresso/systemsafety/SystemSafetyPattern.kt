525a5e6984fc13decc7c158528bb50bd5273ffa0
Areyana <34141661+Areyana@users.noreply.github.com>
2023-07-31 12:51:59 +0300

    TECH: try to fix tests on Api30

--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/systemsafety/SystemSafetyPattern.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/systemsafety/SystemSafetyPattern.kt
@@ -4,5 +4,6 @@ import java.util.regex.Pattern
 
 enum class SystemSafetyPattern(val pattern: Pattern) {
     API23(Pattern.compile("\\S*android.packageinstaller\\S*")),
+    API30(Pattern.compile("\\S*android.permissioncontroller\\S*")),
     OTHER(Pattern.compile("\\S*google.android\\S*"))
 }
