b945494efd359b157668bc6bdad61ce3866c2df9
Maria Glushchenko <jealousmary@mail.ru>
2023-08-01 14:59:47 +0300

    ISSUE-543: add check for layout title in metadata file

--- samples/kaspresso-sample/src/androidTest/kotlin/com/kaspersky/kaspressample/docloc_tests/ScreenshotActivityWithToolbarTest.kt
+++ samples/kaspresso-sample/src/androidTest/kotlin/com/kaspersky/kaspressample/docloc_tests/ScreenshotActivityWithToolbarTest.kt
@@ -5,17 +5,37 @@ import androidx.test.ext.junit.rules.activityScenarioRule
 import com.kaspersky.kaspressample.MainActivity
 import com.kaspersky.kaspressample.screen.MainScreen
 import com.kaspersky.kaspressample.screen.WithToolbarScreen
+import com.kaspersky.kaspresso.files.dirs.DirsProvider
+import com.kaspersky.kaspresso.files.resources.ResourcesRootDirsProvider
+import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.DocLocScreenshotTestCase
+import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 
-class ScreenshotActivityWithToolbarTest : DocLocScreenshotTestCase(locales = "en") {
+private lateinit var testDirsProvider: DirsProvider
+private lateinit var testRootDirsProvider: ResourcesRootDirsProvider
+
+class ScreenshotActivityWithToolbarTest : DocLocScreenshotTestCase(
+    locales = LOCALE,
+    kaspressoBuilder = Kaspresso.Builder.simple().apply {
+        testDirsProvider = dirsProvider
+        testRootDirsProvider = resourcesRootDirsProvider
+    }
+) {
+
+    companion object {
+        private const val SCREENSHOT_NAME = "screen_with_toolbar"
+        private const val SCREENSHOTS_SUBDIR =
+            "com.kaspersky.kaspressample.docloc_tests.ScreenshotActivityWithToolbarTest/test"
+        private const val LOCALE = "en"
+    }
 
     @get:Rule
     val activityRule = activityScenarioRule<MainActivity>()
 
     @Test
-    fun takeScreenshot() = run {
+    fun test() = run {
         step("Open screen with toolbar") {
             MainScreen {
                 withToolbarButton {
@@ -30,7 +50,16 @@ class ScreenshotActivityWithToolbarTest : DocLocScreenshotTestCase(locales = "en
                     view.perform(ViewActions.swipeUp())
                 }
             }
-            captureScreenshot("screen with toolbar")
+            captureScreenshot(SCREENSHOT_NAME)
+        }
+
+        step("Check for toolbar layout title in metadata") {
+            val screenshotsRootDir = testDirsProvider.provideNew(testRootDirsProvider.screenshotsRootDir)
+            val metadataFile = screenshotsRootDir.resolve("$LOCALE/$SCREENSHOTS_SUBDIR/$SCREENSHOT_NAME.xml")
+            assertTrue(metadataFile.exists())
+
+            val contents = metadataFile.readText()
+            assertTrue("toolbar layout title" in contents)
         }
     }
 }
