1ec08c2c3122cc50b9b92f15837f5e05d93b2ec5
Azamat Cherchesov <a1tt@rambler.ru>
2023-08-01 13:18:36 +0300

    TECH: test

--- samples/kaspresso-sample/src/androidTest/kotlin/com/kaspersky/kaspressample/docloc_tests/ScreenshotSampleTest.kt
+++ samples/kaspresso-sample/src/androidTest/kotlin/com/kaspersky/kaspressample/docloc_tests/ScreenshotSampleTest.kt
@@ -14,6 +14,7 @@ import org.junit.Test
 /**
  * An example of [DocLocScreenshotTestCase] usage.
  * For more information see DocLoc wiki page.
+ * .
  */
 class ScreenshotSampleTest : DocLocScreenshotTestCase(locales = "en,ru") {
 
@@ -66,4 +67,3 @@ class ScreenshotSampleTest : DocLocScreenshotTestCase(locales = "en,ru") {
         }
     }
 }
-
