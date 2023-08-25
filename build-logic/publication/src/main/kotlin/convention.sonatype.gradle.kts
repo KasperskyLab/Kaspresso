7a510a6b551649861e6372617cb3280c645a06d4
Azamat Cherchesov <a1tt@rambler.ru>
2023-08-25 13:19:06 +0300

    TECH: reverted tmp commented signing checks

--- build-logic/publication/src/main/kotlin/convention.sonatype.gradle.kts
+++ build-logic/publication/src/main/kotlin/convention.sonatype.gradle.kts
@@ -41,10 +41,10 @@ signing {
     useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
 }
 
-//tasks.withType<Sign>().configureEach {
-//    onlyIf {
-//        val isReleaseQueued = gradle.taskGraph.hasTask("publishAllPublicationsTo${sonatypeReleasesRepoName}Repository")
-//        val isSnapshotQueued = gradle.taskGraph.hasTask("publishAllPublicationsTo${sonatypeSnapshotsRepoName}Repository")
-//        isReleaseQueued || isSnapshotQueued
-//    }
-//}
+tasks.withType<Sign>().configureEach {
+    onlyIf {
+        val isReleaseQueued = gradle.taskGraph.hasTask("publishAllPublicationsTo${sonatypeReleasesRepoName}Repository")
+        val isSnapshotQueued = gradle.taskGraph.hasTask("publishAllPublicationsTo${sonatypeSnapshotsRepoName}Repository")
+        isReleaseQueued || isSnapshotQueued
+    }
+}
