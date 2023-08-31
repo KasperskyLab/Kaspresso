e58d7f8b5bf9dfb8965bc2d90c3e29299b482ab7
sumin93 <suminfx@mail.ru>
2023-08-31 13:27:23 +0300

    ISSUE-372: add real loading data

--- tutorial/src/main/kotlin/com/kaspersky/kaspresso/tutorial/user/LoadUserRepository.kt
+++ tutorial/src/main/kotlin/com/kaspersky/kaspresso/tutorial/user/LoadUserRepository.kt
@@ -1,27 +1,8 @@
 package com.kaspersky.kaspresso.tutorial.user
 
-import kotlinx.coroutines.delay
-
 object LoadUserRepository {
 
-    private val errors = arrayOf(false, false, true)
-    private val users = arrayOf(
-        User(name = "John", lastName = "Smith"),
-        User(name = "Max", lastName = "Thomson"),
-        User(name = "Helen", lastName = "West"),
-    )
+    private val apiService = ApiFactory.apiService
 
-    private var lastIndex = 0
-
-    suspend fun loadUser(): User {
-        delay(3000)
-        val index = lastIndex % errors.size
-        lastIndex++
-        val isError = errors[index]
-        if (!isError) {
-            return users[index]
-        } else {
-            throw RuntimeException("Error when getting user")
-        }
-    }
+    suspend fun loadUser(): User = apiService.loadUser()
 }
