069fe7ea698d3f06381173bfd64cd1d5da2f9bb4
mfglushchenko <jealousmary@mail.ru>
2023-12-04 15:53:08 +0300

    setup gradle plugin portal publishing

--- kaspresso-plugin/build.gradle.kts
+++ kaspresso-plugin/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
+    id("com.gradle.plugin-publish") version "1.2.1"
 }
 
 dependencies {
@@ -9,13 +10,28 @@ dependencies {
     implementation(projects.adbServer.adbserverDesktop)
 }
 
-// TODO setup maven publication
 group = "com.kaspersky.kaspresso"
+version = "1.0"
 gradlePlugin {
+    website.set("https://kasperskylab.github.io/Kaspresso/en/")
+    vcsUrl.set("https://github.com/KasperskyLab/Kaspresso/")
+
     plugins {
-        create("SignerPlugin") {
-            id = "com.kaspersky.kaspresso"
+        create("AdbServerPlugin") {
+            id = "com.kaspersky.kaspresso-adb-server-plugin"
+            displayName = "Kaspresso ADB-server plugin"
+            description = "Run Kaspresso ADB server for Android UI tests"
+            tags.set(listOf("testing", "UI tests", "test automation", "android", "kasresso", "adb server"))
             implementationClass = "com.kaspersky.kaspresso.plugin.KaspressoPlugin"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "localPluginRepository"
+            url = uri("../local-plugin-repository")
         }
     }
 }
