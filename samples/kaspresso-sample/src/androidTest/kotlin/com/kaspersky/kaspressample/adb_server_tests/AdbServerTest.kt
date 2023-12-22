f3b688a9da0ed20d9f5477971f4ac2a65856cfb9
Nikita Evdokimov <nikita-e57@yandex.ru>
2023-12-22 15:36:10 +0300

    ISSUE-576: Add test

---
+++ samples/kaspresso-sample/src/androidTest/kotlin/com/kaspersky/kaspressample/adb_server_tests/AdbServerTest.kt
@@ -0,0 +1,38 @@
+package com.kaspersky.kaspressample.adb_server_tests
+
+import android.Manifest
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.rule.GrantPermissionRule
+import com.kaspersky.kaspressample.MainActivity
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.Rule
+import org.junit.Test
+
+class AdbServerTest : TestCase() {
+    @get:Rule
+    val runtimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
+
+    @get:Rule
+    val activityRule = activityScenarioRule<MainActivity>()
+
+    @Test
+    fun singleCommandTest() = run {
+        val devices = adbServer.performAdb("devices", arguments = emptyList())
+        assert(devices.isNotEmpty())
+    }
+
+    @Test
+    fun commandWithArgumentsTest() = run {
+        adbServer.performShell("mkdir", listOf("-p", "\"/sdcard/Documents/path with whitespace/dir\""))
+        adbServer.performShell("touch", arguments = listOf("\"/sdcard/Documents/path with whitespace/dir/myfile.txt\""))
+        adbServer.performShell("rm", arguments = listOf("\"/sdcard/Documents/path with whitespace/dir/myfile.txt\""))
+    }
+
+    @Test
+    fun commandWithAnotherCommandAsArgumentsTest() = run {
+        adbServer.performCmd("sh", listOf("-c", "adb shell dumpsys deviceidle | grep mForceIdle"))
+    }
+}
