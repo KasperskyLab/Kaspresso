bef189404dd62d7f065df3a22abc6e37f1197574
Nikita Evdokimov <nikita-e57@yandex.ru>
2023-12-28 13:50:36 +0300

    TECH: bump kotlin

--- build-logic/kotlin/src/main/kotlin/convention.kotlin-base.gradle.kts
+++ build-logic/kotlin/src/main/kotlin/convention.kotlin-base.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlinLanguageVersion = "1.7"
+val kotlinLanguageVersion = "1.9"
 
 tasks.withType<KotlinCompile>().configureEach {
     kotlinOptions {
