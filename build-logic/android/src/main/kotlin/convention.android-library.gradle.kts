4fdd1a87edef69d1dc918dcc5f22ea6e095ae9d2
Azamat <azamat.cherchesov@kaspersky.com>
2024-02-21 16:24:52 +0300

    TECH: deleted target 17

--- build-logic/android/src/main/kotlin/convention.android-library.gradle.kts
+++ build-logic/android/src/main/kotlin/convention.android-library.gradle.kts
@@ -21,13 +21,13 @@ android {
     }
 
     compileOptions {
-        targetCompatibility = JavaVersion.VERSION_11
-        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
 tasks.withType<KotlinCompile>().configureEach {
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 }
