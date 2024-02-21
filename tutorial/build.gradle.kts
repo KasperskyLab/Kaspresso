02bb523fa99d960d1c662eee2b76755b39c2a600
Azamat <azamat.cherchesov@kaspersky.com>
2024-02-21 19:06:43 +0300

    TECH: deleted target 17

--- tutorial/build.gradle.kts
+++ tutorial/build.gradle.kts
@@ -24,7 +24,7 @@ android {
 
     java {
         toolchain {
-            languageVersion.set(JavaLanguageVersion.of(8))
+            languageVersion.set(JavaLanguageVersion.of(9))
         }
     }
 }
