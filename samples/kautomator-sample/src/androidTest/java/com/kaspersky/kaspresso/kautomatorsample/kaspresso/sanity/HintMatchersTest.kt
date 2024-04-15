b6abc76b0cd88f687c4fa7405c7aeddd0b4688f5
Nikita Evdokimov <nikita-e57@yandex.ru>
2024-04-15 12:49:23 +0300

    ISSUE-630: Skip hint matchers test on old APIs

--- samples/kautomator-sample/src/androidTest/java/com/kaspersky/kaspresso/kautomatorsample/kaspresso/sanity/HintMatchersTest.kt
+++ samples/kautomator-sample/src/androidTest/java/com/kaspersky/kaspresso/kautomatorsample/kaspresso/sanity/HintMatchersTest.kt
@@ -1,9 +1,11 @@
 package com.kaspersky.kaspresso.kautomatorsample.kaspresso.sanity
 
+import android.os.Build
 import androidx.test.ext.junit.rules.activityScenarioRule
 import com.kaspersky.kaspresso.kautomatorsample.ComponentsActivity
 import com.kaspersky.kaspresso.kautomatorsample.screen.ComponentsScreen
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.Assume
 import org.junit.Rule
 import org.junit.Test
 
@@ -14,6 +16,8 @@ class HintMatchersTest : TestCase() {
 
     @Test
     fun test() = run {
+        Assume.assumeTrue("Hint matchers are available on API 26+", Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+
         ComponentsScreen {
             editTextByHintResId { isDisplayed() }
             editTextByHintText { isDisplayed() }
