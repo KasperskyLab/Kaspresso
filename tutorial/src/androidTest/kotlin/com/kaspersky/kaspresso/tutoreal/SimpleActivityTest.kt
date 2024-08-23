e768d84fe389d2edbaf279becc989fada9ac3411
Xqluziff <xqluziff@yandex.ru>
2024-08-23 14:59:00 +0300

    TECH:SimpleActivityTest-OK

--- tutorial/src/androidTest/kotlin/com/kaspersky/kaspresso/tutoreal/SimpleActivityTest.kt
+++ tutorial/src/androidTest/kotlin/com/kaspersky/kaspresso/tutoreal/SimpleActivityTest.kt
@@ -1,4 +1,41 @@
 package com.kaspersky.kaspresso.tutoreal
 
-class SimpleActivityTest {
+import androidx.test.ext.junit.rules.activityScenarioRule
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import com.kaspersky.kaspresso.tutoreal.screen.MainSceen
+import com.kaspersky.kaspresso.tutoreal.screen.SimpleActivityScreen
+import com.kaspersky.kaspresso.tutorial.MainActivity
+import com.kaspersky.kaspresso.tutorial.R
+import org.junit.Rule
+import org.junit.Test
+
+class SimpleActivityTest: TestCase() {
+
+    @get:Rule
+    val activityRule = activityScenarioRule<MainActivity>()
+
+    @Test
+    fun test(){
+
+        MainSceen{
+            simpleActivityButton{
+                isVisible()
+                isClickable()
+                click()
+            }
+        }
+
+        SimpleActivityScreen{
+
+            simpleTitle.isVisible()
+            changeTitleButton.isClickable()
+            simpleTitle.hasText(R.string.simple_activity_default_title)
+            inputText.replaceText("New")
+            changeTitleButton.click()
+            simpleTitle.hasText("New")
+        }
+
+    }
+
+
 }
