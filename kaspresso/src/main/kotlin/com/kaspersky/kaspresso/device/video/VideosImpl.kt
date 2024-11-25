b01fd581c105a3c01251cabf40274847d71cfaa6
ersanin <ersanin@gmail.com>
2024-11-26 01:02:14 +0300

    ISSUE-675: review reaction

--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/device/video/VideosImpl.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/device/video/VideosImpl.kt
@@ -12,9 +12,10 @@ class VideosImpl(
 ) : Videos {
 
     override fun record(tag: String) {
-        val sanitizedVideoName = tag
-            .replace("[/:\"*?<>|]+".toRegex(), "")
-            .replace(' ', '_')
+        val sanitizedVideoName = tag.replace("[/:\"*?<>|]+".toRegex(), "_")
+        if (tag != sanitizedVideoName) {
+            logger.d("Can't record video with name $tag since it contains one of the following special characters [/:\"*?<>|]. Changing the name to $sanitizedVideoName")
+        }
         val videoFile: File = resourceFilesProvider.provideVideoFile(sanitizedVideoName)
         videoRecorder.start(videoFile)
     }
