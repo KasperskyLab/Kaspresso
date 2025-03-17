75a5f4b35b278bcd87c6d1cbbdd919521ec54b8c
Nikita Evdokimov <nikita-e57@yandex.ru>
2025-03-17 17:05:59 +0300

    TECH: Disable test on the lower APIs

--- samples/kaspresso-sample/src/androidTest/kotlin/com/kaspersky/kaspressample/visual/VisualTestSample.kt
+++ samples/kaspresso-sample/src/androidTest/kotlin/com/kaspersky/kaspressample/visual/VisualTestSample.kt
@@ -1,11 +1,14 @@
 package com.kaspersky.kaspressample.visual
 
 import android.Manifest
+import android.os.Build
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.rule.GrantPermissionRule
 import com.kaspersky.kaspressample.MainActivity
 import com.kaspersky.kaspressample.screen.MainScreen
 import com.kaspersky.kaspresso.testcases.api.testcase.VisualTestCase
+import org.junit.Assume
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 
@@ -21,7 +24,11 @@ class VisualTestSample : VisualTestCase() {
     val activityRule = activityScenarioRule<MainActivity>()
 
     @Test
-    fun test() = runScreenshotTest {
+    fun test() = runScreenshotTest{
+        Assume.assumeTrue(
+            "Granting READ_MEDIA_IMAGES fails on the lower APIs",
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+        )
         step("Open Simple Screen") {
             MainScreen {
                 simpleButton {
