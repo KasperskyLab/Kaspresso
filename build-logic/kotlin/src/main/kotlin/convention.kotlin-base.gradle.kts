6ff96857155fc0edd16a3055a87391ea611035e1
Nikita Evdokimov <nikita-e57@yandex.ru>
2025-12-08 16:51:18 +0300

    TECH: Bump java

--- build-logic/kotlin/src/main/kotlin/convention.kotlin-base.gradle.kts
+++ build-logic/kotlin/src/main/kotlin/convention.kotlin-base.gradle.kts
@@ -1,18 +1,15 @@
 
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlinLanguageVersion = "1.8"
 
 tasks.withType<KotlinCompile>().configureEach {
     kotlinOptions {
 
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
 
 //        TODO: fix all warnings before
 //        allWarningsAsErrors = true
 
-        languageVersion = kotlinLanguageVersion
-        apiVersion = kotlinLanguageVersion
 
         freeCompilerArgs = freeCompilerArgs +
                 "-opt-in=kotlin.RequiresOptIn" +
@@ -21,6 +18,6 @@ tasks.withType<KotlinCompile>().configureEach {
 }
 
 tasks.withType<JavaCompile>().configureEach {
-    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
-    targetCompatibility = JavaVersion.VERSION_1_8.toString()
+    sourceCompatibility = JavaVersion.VERSION_11.toString()
+    targetCompatibility = JavaVersion.VERSION_11.toString()
 }
