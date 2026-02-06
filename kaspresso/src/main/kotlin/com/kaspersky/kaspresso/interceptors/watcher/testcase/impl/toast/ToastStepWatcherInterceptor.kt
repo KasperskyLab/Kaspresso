e5b8988c54726e9523a46c55040f8a07f7511e29
Nikita Evdokimov <nikita-e57@yandex.ru>
2026-02-06 16:05:36 +0300

    ISSUE-730: Fix main thread exception; allow custom toast content

--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/interceptors/watcher/testcase/impl/toast/ToastStepWatcherInterceptor.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/interceptors/watcher/testcase/impl/toast/ToastStepWatcherInterceptor.kt
@@ -1,34 +1,44 @@
 package com.kaspersky.kaspresso.interceptors.watcher.testcase.impl.toast
 
 import android.content.Context
+import android.os.Handler
 import android.widget.Toast
-import androidx.test.uiautomator.UiDevice
 import com.kaspersky.kaspresso.interceptors.watcher.testcase.StepWatcherInterceptor
 import com.kaspersky.kaspresso.testcases.models.info.StepInfo
 import org.jetbrains.annotations.ApiStatus
 
+/**
+ * Shows a toast with step info before each step execution.
+ */
 @ApiStatus.Experimental
 class ToastStepWatcherInterceptor(
     private val context: Context,
-    private val toastContent: ToastContent = ToastContent.DESCRIPTION,
+    private val toastContentProvider: (StepInfo) -> CharSequence = ToastContentProviders.description,
 ) : StepWatcherInterceptor {
     override fun interceptBefore(stepInfo: StepInfo) {
-        when (toastContent) {
-            ToastContent.NUMBER -> toast("Step #${getNumber(stepInfo)}")
-            ToastContent.DESCRIPTION -> toast(stepInfo.description)
-            ToastContent.FULL_INFO -> toast(
-                "${getNumber(stepInfo)} ${stepInfo.description}"
-            )
+        toast(toastContentProvider.invoke(stepInfo))
+    }
+
+    private fun toast(content: CharSequence) {
+        Handler(context.mainLooper).post {
+            Toast.makeText(context.applicationContext, content, Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun toast(content: CharSequence) = Toast.makeText(context.applicationContext, content, Toast.LENGTH_SHORT).show()
+    /**
+     * A set of default implementations of a toast content provider
+     */
+    object ToastContentProviders {
+        val description: (StepInfo) -> CharSequence = { stepInfo ->
+            stepInfo.description
+        }
+        val number: (StepInfo) -> CharSequence = { stepInfo ->
+            "Step #${getNumber(stepInfo)}"
+        }
+        val fullInfo: (StepInfo) -> CharSequence = { stepInfo ->
+            "${getNumber(stepInfo)} ${stepInfo.description}"
+        }
 
-    private fun getNumber(stepInfo: StepInfo) = stepInfo.number ?: stepInfo.ordinal.toString()
-
-    enum class ToastContent {
-        DESCRIPTION,
-        NUMBER,
-        FULL_INFO
+        private fun getNumber(stepInfo: StepInfo) = stepInfo.number ?: stepInfo.ordinal.toString()
     }
 }
