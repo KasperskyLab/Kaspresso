4f1d14b2dc9a56d34e9fdedb0b4f737088c50bad
Nikita Evdokimov <nikita-e57@yandex.ru>
2026-03-02 18:55:52 +0300

    TECH: Add CVE includes

--- build-logic/third-party-report/src/main/kotlin/convention.third-party-report.gradle.kts
+++ build-logic/third-party-report/src/main/kotlin/convention.third-party-report.gradle.kts
@@ -1,7 +1,29 @@
+import com.kaspersky.gradle.thirdpartyreport.model.CveAction
+
 // Please do not remove this file. This file is used for new releases.
 plugins {
     id("com.kaspersky.gradle.third-party-report-plugin")
+    id("com.kaspersky.gradle.third-party-report-include-plugin")
 }
 
 thirdPartyReport {
+}
+
+thirdPartyReportInclude {
+    includeCve(
+        name = "org.jsoup:jsoup",
+        parentId = "CVE-2021-37714",
+        action = CveAction.NotApplicable,
+        description = "CVE про denial of service вэб страниц. Kaspresso - это фреймворк для UI тестирования Android приложений. " +
+                "В рамках его функциональности не используется парсинг html страниц. В production коде Kaspresso не может использоваться"
+    )
+    includeCve(
+        name = "org.jsoup:jsoup",
+        parentId = "CVE-2022-36033",
+        action = CveAction.NotApplicable,
+        description = "Согласно описанию CVE есть риск атаки XSS. В случае мобильных приложений это возможно только в случае тестирования элементов WebView. " +
+                "Однако, библиотеку org.jsoup:jsoup.1.15.1 использует Accessibility Test Framework for Android https://github.com/google/Accessibility-Test-Framework-for-Android. " +
+                "Он используется для осуществления тестовых проверок для элементов пользовательского интерфейса, однако, в случае WebView Accessibility не может получить html сайта. " +
+                "А это значит, что и XSS осуществить невозможно"
+    )
 }
