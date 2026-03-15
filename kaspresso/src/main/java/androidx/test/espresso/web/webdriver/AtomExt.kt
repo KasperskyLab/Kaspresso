6e5ebfe6206940cfcce7c66ce28f12b927e4218a
Nikita Evdokimov <nikita-e57@yandex.ru>
2026-03-15 20:39:54 +0300

    TECH: License mentions

--- kaspresso/src/main/java/androidx/test/espresso/web/webdriver/AtomExt.kt
+++ kaspresso/src/main/java/androidx/test/espresso/web/webdriver/AtomExt.kt
@@ -13,6 +13,25 @@ import androidx.test.espresso.web.webdriver.WebDriverAtomScripts.GET_VISIBLE_TEX
 import androidx.test.espresso.web.webdriver.WebDriverAtomScripts.SCROLL_INTO_VIEW_ANDROID
 import androidx.test.espresso.web.webdriver.WebDriverAtomScripts.SEND_KEYS_ANDROID
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 /**
  * Uses [WebDriverAtomScripts] class, that has package-local access in Espresso, so it has to be in the same package.
  *
--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/params/SystemDialogsSafetyParams.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/params/SystemDialogsSafetyParams.kt
@@ -2,6 +2,25 @@ package com.kaspersky.kaspresso.params
 
 import java.util.concurrent.TimeUnit
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 /**
  * @param shouldIgnoreKeyboard by default kaspresso treats the keyboard as a system dialog. Enabling this flag prevents a keyboard closing
  * @param shouldIgnoreCrashes whether kaspresso should check if there's a system crash dialog. Be aware that enabling it increases a time spent
