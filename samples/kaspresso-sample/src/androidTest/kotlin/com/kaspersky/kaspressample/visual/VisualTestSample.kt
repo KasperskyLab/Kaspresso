53d7b6b3ea3d7f14fba9e5746e525b351df7bde0
Nikita <nikita-e57@yandex.ru>
2026-04-07 18:51:27 +0300

    TECH: Fix visual test sample on API 30 (#739)
    * TECH: Fix visual test sample on API 30

--- samples/kaspresso-sample/src/androidTest/kotlin/com/kaspersky/kaspressample/visual/VisualTestSample.kt
+++ samples/kaspresso-sample/src/androidTest/kotlin/com/kaspersky/kaspressample/visual/VisualTestSample.kt
@@ -7,27 +7,30 @@ import androidx.test.rule.GrantPermissionRule
 import com.kaspersky.kaspressample.MainActivity
 import com.kaspersky.kaspressample.screen.MainScreen
 import com.kaspersky.kaspresso.testcases.api.testcase.VisualTestCase
-import org.junit.Assume
 import org.junit.Rule
 import org.junit.Test
 
 class VisualTestSample : VisualTestCase() {
+
     @get:Rule
-    val runtimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
-        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-        Manifest.permission.READ_EXTERNAL_STORAGE,
-        Manifest.permission.READ_MEDIA_IMAGES,
-    )
+    val runtimePermissionRule: GrantPermissionRule = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        GrantPermissionRule.grant(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.READ_MEDIA_IMAGES,
+        )
+    } else {
+        GrantPermissionRule.grant(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+        )
+    }
 
     @get:Rule
     val activityRule = activityScenarioRule<MainActivity>()
 
     @Test
     fun test() = runScreenshotTest {
-        Assume.assumeTrue(
-            "Granting READ_MEDIA_IMAGES fails on the lower APIs",
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
-        )
         step("Open Simple Screen") {
             MainScreen {
                 simpleButton {
