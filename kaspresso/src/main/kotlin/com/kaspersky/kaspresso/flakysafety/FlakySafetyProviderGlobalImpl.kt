1200d301995c5ffc7ec73ab0c40b221e80c45ebf
Nikita <nikita-e57@yandex.ru>
2026-04-07 19:37:06 +0300

    ISSUE-647: Restore interceptors on exception in flaky safety (#741)
    * ISSUE-647: Restore interceptors on exception in flaky safety
    
    (cherry picked from commit c473873d35ed7cb804150e011e810871ba0dbfd4)

--- kaspresso/src/main/kotlin/com/kaspersky/kaspresso/flakysafety/FlakySafetyProviderGlobalImpl.kt
+++ kaspresso/src/main/kotlin/com/kaspersky/kaspresso/flakysafety/FlakySafetyProviderGlobalImpl.kt
@@ -58,11 +58,15 @@ class FlakySafetyProviderGlobalImpl(
     override fun <T> flakySafely(action: () -> T): T {
         flakySafeInterceptorScalpel.scalpFromLibs()
 
-        val result = flakySafetyAlgorithm.invokeFlakySafely(
-            params = getParams(),
-            action = action
-        )
-
+        val result = try {
+            flakySafetyAlgorithm.invokeFlakySafely(
+                params = getParams(),
+                action = action
+            )
+        } catch (ex: Throwable) {
+            flakySafeInterceptorScalpel.restoreScalpToLibs()
+            throw ex
+        }
         flakySafeInterceptorScalpel.restoreScalpToLibs()
 
         return result
@@ -91,12 +95,16 @@ class FlakySafetyProviderGlobalImpl(
     ): T {
         flakySafeInterceptorScalpel.scalpFromLibs()
 
-        val result = flakySafetyAlgorithm.invokeFlakySafely(
-            params = getParams(timeoutMs, intervalMs, allowedExceptions),
-            failureMessage = failureMessage,
-            action = action
-        )
-
+        val result = try {
+            flakySafetyAlgorithm.invokeFlakySafely(
+                params = getParams(timeoutMs, intervalMs, allowedExceptions),
+                failureMessage = failureMessage,
+                action = action
+            )
+        } catch (ex: Throwable) {
+            flakySafeInterceptorScalpel.restoreScalpToLibs()
+            throw ex
+        }
         flakySafeInterceptorScalpel.restoreScalpToLibs()
 
         return result
